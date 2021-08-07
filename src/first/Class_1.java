862871fb9791547107ad00648e7702fd5e1dead8
ilqar <ilqar.nureddinoglu@gmail.com>
2021-08-07 14:06:12 -0400

    first

---
+++ src/first/Class_1.java
@@ -0,0 +1,18 @@
+package first;
+
+public class Class_1 {
+
+    public static void main(String[] args) {
+
+        String str = "Ilgar Ali";
+        String nonObj = "";
+        for (int i = str.length()-1; i>=0; i--){
+            nonObj += str.toCharArray()[i];
+        }
+        System.out.println(nonObj);
+
+    }
+
+
+
+}

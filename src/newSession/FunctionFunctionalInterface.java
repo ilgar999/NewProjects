358c81e467823ba16a57aa7480101f149373fa1d
ilqar <ilqar.nureddinoglu@gmail.com>
2021-09-01 22:13:04 -0400

    Function.Supplier 01

--- src/newSession/FunctionFunctionalInterface.java
+++ src/newSession/FunctionFunctionalInterface.java
@@ -1,6 +1,8 @@
 package newSession;
 
+import java.util.Arrays;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class FunctionFunctionalInterface {
 
@@ -8,25 +10,49 @@ public class FunctionFunctionalInterface {
 
         Function<String, String> reverse = s ->{
             String result = "";
-            for (int i = s.length(); i>0; i--)
+            for(int i = s.length()-1; i >=0; i--)
                 result += s.charAt(i);
             return result;
         };
 
-        String str1 = reverse.apply("Cybertek" );
+        String str1 = reverse.apply("Cybertek");
         System.out.println(str1);
 
-        System.out.println("-------------------------------------------");
+        System.out.println("-------------------------------------");
+
+        Function<String, Boolean> isPalindrome = s -> reverse.apply(s).equalsIgnoreCase(s);
+
+        boolean r = isPalindrome.apply("Anna");
+
+        System.out.println(r);
+
+        System.out.println("-------------------------------------");
 
         Function<int[], int[]> reverseIntArray = a ->{
             int[] result = new int[a.length];
-            for (int i = 0, j = a.length-1; i < a.length; i++, j--){
-                result[j] = a[i] ;
+            for (int i = 0, j =a.length-1; i < a.length; i++, j--) {
+                result[j] = a[i];
             }
             return result;
         };
 
+
         int[] arr1 = {1,2,3,4,5};
         int[] arr2 = reverseIntArray.apply(arr1);
+
+        System.out.println(Arrays.toString(arr2));
+
+        System.out.println("----------------------------------------");
+
+        Integer[] numbers= {1,2,3,4,5,6,7,1,2,3,4,5,6,7,8};
+
+        numbers = Arrays.asList(numbers).stream().distinct().collect(Collectors.toList()).toArray(new Integer[0]);
+
+        System.out.println(Arrays.toString(numbers));
+
+
+
+
     }
+
 }
